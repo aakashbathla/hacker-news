bf80da4fed82200260d0360d3335b5938255d106
aakashbathla <aakash.bathla7@gmail.com>
2020-05-23 03:14:31 +0530

    fitst commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (reg, res) {
+  res.send("Hello Aakash, How are You!");
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("App is listening at http://%s:%s", host, port);
+});
