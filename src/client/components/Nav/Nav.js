4453a3d12435219a92d43237e9a2c47868aeceac
aakashbathla <aakash.bathla7@gmail.com>
2020-05-25 12:26:45 +0530

    mobile nav fixed

--- src/client/components/Nav/Nav.js
+++ src/client/components/Nav/Nav.js
@@ -2,21 +2,25 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="d-flex nav">
-      <img
-        className="nav__image ml25"
-        src="https://news.ycombinator.com/y18.gif"
-        alt="logo image"
-      />
-      <span className="nav__title ml25">Hacker News</span>
-      <span className="nav__links ml25">
-        <a className="active" href="top">
-          Top |
-        </a>
-        <a className="ml10" href="new">
-          New
-        </a>
-      </span>
+    <div className="d-flex nav nav__mobile">
+      <div>
+        <img
+          className="nav__image ml25"
+          src="https://news.ycombinator.com/y18.gif"
+          alt="logo image"
+        />
+        <span className="nav__title ml25">Hacker News</span>
+      </div>
+      <div className="nav__mobile--padding">
+        <span className="nav__links ml25">
+          <a className="active" href="top">
+            Top |
+          </a>
+          <a className="ml10" href="new">
+            New
+          </a>
+        </span>
+      </div>
     </div>
   );
 };
