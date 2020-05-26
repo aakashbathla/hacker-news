19ec5fe090108b143fa866cfd3bdb4cd8b44ffab
aakashbathla <aakash.bathla7@gmail.com>
2020-05-26 12:11:53 +0530

    code refactoring for speed

--- src/server/template.js
+++ src/server/template.js
@@ -1,12 +1,12 @@
 export default function renderFullPage(html, preloadedState) {
   return `
     <!doctype html>
-    <html>
+    <html lang="en-GB">
       <head>
         <title>Hacker News App</title>
         <meta name="description" content="Hacker News is a community started by Paul Graham for sharing "Anything that good hackers would find interesting. That includes more than hacking and startups. If you had to reduce it to a sentence, the answer might be: anything that gratifies one's intellectual curiosity."">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link rel="prefetch" href="./main.css">
+        <link rel="stylesheet" href="./main.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
       </head>
       <body>
